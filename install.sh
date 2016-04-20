98b4cecba0f3edcc1bac3476b992e94cb96ea1bc
Ryan Schulman <schulmanworks@gmail.com>
2016-04-20 13:01:57 -0600

    made install shell script

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python3-numpy python3-scipy python3-matplotlib ipython3 ipython3-notebook python3-pandas  python3-nose
+sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0rc0-cp34-cp34m-linux_x86_64.whl
+sudo pip3 install -U scikit-learn
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0rc0-cp34-cp34m-linux_x86_64.whl
+sudo pip install -U scikit-learn
